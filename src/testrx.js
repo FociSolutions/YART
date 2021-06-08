6c3d47b6dbdcac8071cde7155193f562dd2a1d60
waltiam <2963975+waltiam@users.noreply.github.com>
2021-06-08 23:38:35 +0000

    :memo::truck:fire: got rid of the manual html page

--- src/testrx.js
+++ src/testrx.js
@@ -1,4 +1,11 @@
-import { of } from 'rxjs';
+import { from, of } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { max } from 'rxjs/operators';
 
 map(x => x * x)(of(1, 2, 3)).subscribe((v) => console.log(`Output is: ${v}`));
+
+let list1 = [1, 6, 15, 10, 58, 2, 40];
+from(list1)
+    .pipe(max((a, b) => a - b))
+    .subscribe(x => console.log("The max value is " + x)
+    );
